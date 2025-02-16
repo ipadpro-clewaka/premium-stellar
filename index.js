8fb0ca31d8c4575ae9e057eab877c69e531e5206
ipadpro-clewaka <gomimaedakitaare@gmail.com>
2025-02-16 19:37:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const __dirname = process.cwd()
 const server = http.createServer()
 const app = express()
 const bareServer = createBareServer("/ov/")
+const ejs = require("ejs")
 const PORT = process.env.PORT || 8080
 if (config.challenge) {
   console.log(`Password protection is enabled. Usernames are: ${Object.keys(config.users)}`)
@@ -93,3 +94,72 @@ server.on("listening", () => {
   console.log(`Running at http://localhost:${PORT}`)
 })
 server.listen({ port: PORT })
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+//レギュラー
+app.get('/w/:id/5.pdf', async (req, res) => {
+    const videoId = req.params.id;
+    const server = req.query.server || '0';
+    const serverUrls = {
+        '0': [
+        'https://natural-voltaic-titanium.glitch.me',
+        'https://wtserver3.glitch.me',
+        'https://wtserver1.glitch.me',
+        'https://wtserver2.glitch.me',
+        ],
+        '1': 'https://wataamee.glitch.me',
+        '2': 'https://watawatawata.glitch.me',
+        '3': 'https://amenable-charm-lute.glitch.me',
+        '4': 'https://wtserver2.glitch.me',
+        '5': 'https://wtserver1.glitch.me',
+        "6": "https://battle-deciduous-bear.glitch.me",
+        "7": 'https://productive-noon-van.glitch.me',
+	"8": 'https://balsam-secret-fine.glitch.me',
+    };
+
+    let baseUrl;
+    if (server === '0') {
+        const randomIndex = Math.floor(Math.random() * serverUrls['0'].length);
+        baseUrl = serverUrls['0'][randomIndex];
+    } else {
+        baseUrl = serverUrls[server] || 'https://wtserver1.glitch.me';
+    }
+  
+    if (!/^[a-zA-Z0-9_-]{11}$/.test(videoId)) {
+        return res.status(400).send('videoIDが正しくありません');
+    }
+
+    const cookies = parseCookies(req);
+    const wakames = cookies.wakametubeumekomi === 'true';
+    if (wakames) {
+        return res.redirect(`/umekomi/${videoId}`);
+    }
+    try {
+        console.log(baseUrl);
+        const response = await axios.get(`${baseUrl}/api/${videoId}`);
+        const videoData = response.data;
+        console.log(videoData);
+
+        res.render('sand-smoke-video', { videoData, videoId, baseUrl });
+  } catch (error) {
+        res.status(500).render('matte', { 
+      videoId, baseUrl,
+      error: '動画を取得できません', 
+      details: error.message 
+    });
+  }
+});
+
+//urlでYouTube動画を探す
+app.get("/urls",(req, res) => {
+  res.render("../views/url.ejs")
+})
+
+  
